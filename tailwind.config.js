d96909f61ff54a78b0815ce0a59f1735242d5a74
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2021-12-19 00:44:43 +0100

    remove open-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,27 @@
-const openColor = require('open-color');
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin")
 
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
-  darkMode: "media", // or 'media' or 'class'
-  theme: {
-    colors: openColor,
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  corePlugins: {
-    container: false,
-  },
-  plugins: [
-    plugin(function ({ addUtilities }) {
-      addUtilities(
-        {
-          '.optimize-legibility': {
-            'text-rendering': 'optimizeLegibility',
-          },
-        },
-        ['responsive', 'hover']
-      );
-    }),
-  ],
-};
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+    darkMode: "media", // or false or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    corePlugins: {
+        container: false,
+    },
+    plugins: [
+        plugin(function ({ addUtilities }) {
+            addUtilities(
+                {
+                    ".optimize-legibility": {
+                        "text-rendering": "optimizeLegibility",
+                    },
+                },
+                ["responsive", "hover"]
+            )
+        }),
+    ],
+}
