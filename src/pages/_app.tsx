73b5f454d3caee53ee3e394d0352970f7f384c72
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2021-12-22 18:50:33 +0100

    chore: clean up _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,9 @@ function App({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <title>Next Starter Lite</title>
             </Head>
-            <div className="subpixel-antialiased bg-white optimize-legibility text-gray-9">
+            <div className="subpixel-antialiased optimize-legibility">
                 <Component {...pageProps} />
             </div>
         </>
