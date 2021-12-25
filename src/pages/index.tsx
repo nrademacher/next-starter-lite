0499e265be15accf9c81ff87de54b4f087edc2e3
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2021-12-25 05:07:27 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="grid place-items-center min-h-screen text-7xl text-blue-7">
+    <h1 className="grid place-items-center min-h-screen text-7xl">
       Hello, world!
     </h1>
   );
