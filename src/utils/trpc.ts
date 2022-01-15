2423587597de05fcc6a6cdbaa75e316a6e3caaea
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-01-15 02:52:28 +0100

    Update trpc.ts

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,6 +1,11 @@
 // utils/trpc.ts
 import { createReactQueryHooks } from '@trpc/react';
 import type { AppRouter } from '../pages/api/trpc/[trpc]';
+import type { inferProcedureOutput } from '@trpc/server';
 
 export const trpc = createReactQueryHooks<AppRouter>();
 // => { useQuery: ..., useMutation: ...}
+
+export type inferQueryOutput<
+  TRouteKey extends keyof AppRouter['_def']['queries'],
+> = inferProcedureOutput<AppRouter['_def']['queries'][TRouteKey]>;
